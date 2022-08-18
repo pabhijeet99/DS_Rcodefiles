062a0257b478ab1af731ae435317645a33097856
Abhijeet Patil <111480872+pabhijeet99@users.noreply.github.com>
2022-08-18 10:44:41 +0530

    Add files via upload

---
+++ Descriptive_Viz.R
@@ -0,0 +1,67 @@
+x=3
+y=6
+z=10
+x=6
+x
+y
+z
+#To get help/description of any function,call the name of the function prefixed with'?'
+?mean
+
+#To get the list of all the objects
+ls()
+
+#To remove the objects from the
+rm()
+
+#Data Types
+a=5
+class(a)
+
+a=5.6
+class()
+
+a=5L
+class(a)
+
+a=5.6L
+class(a)
+
+
+a=True
+a=TRUE
+b=FALSE
+class(a)
+class(b)
+
+m=T
+n=F
+class(m)
+class(n)
+
+a=5+9i
+a
+
+a=5+9
+a
+
+a='Data Science'
+class(a)
+
+
+
+Ozone           Solar.R           Wind             Temp      
+Min.   :  1.00   Min.   :  7.0   Min.   : 1.700   Min.   :56.00  
+1st Qu.: 18.00   1st Qu.:115.8   1st Qu.: 7.400   1st Qu.:72.00  
+Median : 31.50   Median :205.0   Median : 9.700   Median :79.00  
+Mean   : 42.13   Mean   :185.9   Mean   : 9.958   Mean   :77.88  
+3rd Qu.: 63.25   3rd Qu.:258.8   3rd Qu.:11.500   3rd Qu.:85.00  
+Max.   :168.00   Max.   :334.0   Max.   :20.700   Max.   :97.00  
+NA's   :37       NA's   :7                                       
+Month            Day      
+Min.   :5.000   Min.   : 1.0  
+1st Qu.:6.000   1st Qu.: 8.0  
+Median :7.000   Median :16.0  
+Mean   :6.993   Mean   :15.8  
+3rd Qu.:8.000   3rd Qu.:23.0  
+Max.   :9.000   Max.   :31.0  
